0b1941614e54725814fca75aabac0b7c8c4e961e
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-06-03 11:13:36 +0200

    Committing local changes before switching to commit 1101d2f

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,63 +1,60 @@
-<script>
-</script>
-
-<section>
-    <h1>SvelteKit OpenAI</h1>
-</section>
-<nav>
-  <ul class="nav-links">
+<nav class="navbar">
+    <h1 class="title">SvelteKit OpenAI</h1>
+    <ul class="nav-links">
       <li><a href="/">Generador de Imágenes</a></li>
       <li><a href="/chat">ChatBot Gpt</a></li>
     </ul>
-</nav>
-
+  </nav>
   
   <style>
     
-    section {
+    .navbar {
       background-color: #000;
       padding: 1rem;
       display: flex;
       justify-content: center;
       align-items: center;
-     filter: drop-shadow(0 0 20px #fff) drop-shadow(0 0 20px #fff);
+      filter: drop-shadow(0 0 20px #fff) drop-shadow(0 0 20px #fff);
     }
   
-    h1 {
-      font-size: 1.8rem;
+    .title {
+      font-size: 1.5rem;
       margin: 0;
       color: #fff;
-      animation: animate 10s linear infinite;
-        filter: drop-shadow(0 0 20px #fff) drop-shadow(0 0 20px #fff);
-    }
-    @keyframes animate {
-        0% {
-            width: 50%;
-        }
-        100% {
-            width: 80%;
-        }
-    }
-    .nav-links {
-        list-style: none;
-        display: flex;
-        justify-content: flex-end;
-        gap: 1rem;
-        margin-top: 1rem;
-    }
-    .nav-links a {
-        text-decoration: none;
-        color: #fff;
-        padding: 0.5rem 1rem;
-        border-radius: 4px;
-        transition: background-color 0.3s ease;
-        font-size:19px;
-    }
-    .nav-links a:hover {
-        filter: drop-shadow(0 0 20px #fff) drop-shadow(0 0 20px #fff);
-        color:black;
+      animation: animate 5s linear infinite;
     }
   
-
+    .nav-links {
+      list-style: none;
+      display: flex;
+      justify-content: flex-end;
+      gap: 1rem;
+      margin-top: 1rem;
+    }
+  
+    .nav-links li {
+      font-size: 1.2rem;
+    }
+  
+    .nav-links a {
+      text-decoration: none;
+      color: #fff;
+      padding: 0.5rem 1rem;
+      border-radius: 4px;
+      transition: background-color 0.3s ease;
+    }
+  
+    .nav-links a:hover {
+      background-color: #ddd;
+    }
+  
+    @keyframes animate {
+      0% {
+        width: 0;
+      }
+      100% {
+        width: 50%;
+      }
+    }
   </style>
   
