1101d2fe14afbe7a5c1f8b2ccf1ab8f37e60cfe5
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-06-03 10:30:48 +0200

    cambiando estilos al navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script>
     import Nav from "../components/Nav.svelte";
 </script>
-
+<main>
 <Nav />
 <h1>imagen</h1>
+    <h1>imagen</h1>
+    <h1>imagen</h1>
+</main>
+
+
+<style>
+main{
+    box-sizing: border-box;
+    background-color: #111111;
+}
+</style>
